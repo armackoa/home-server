1f17840b8f405ee7101e39a540a01d319dff0700
A Macquet Koagné <archibaldmak@gmail.com>
2026-04-11 13:12:24 +0200

    Update homepage.nix

--- modules/hosts/home-server/homepage.nix
+++ modules/hosts/home-server/homepage.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   flake.nixosModules.homepage = {...}: let
-    domain = "periserver.tail0e3fc3.ts.net";
+    domain = "server.ts.net";
   in {
     services.nginx.virtualHosts."${domain}" = {
       locations."/" = {
@@ -13,27 +13,13 @@
     };
     services.homepage-dashboard = {
       enable = true;
-      allowedHosts = "periserver.tail0e3fc3.ts.net,127.0.0.1:8082,localhost:8082";
+      allowedHosts = "server.ts.net,127.0.0.1:8082,localhost:8082";
       settings = {
-        base = "https://periserver.tail0e3fc3.ts.net/";
+        base = "https://server.ts.net/";
       };
       services = [
         {
           "Media" = [
-            {
-              "Jellyfin" = {
-                icon = "jellyfin.png";
-                href = "https://periserver.tail0e3fc3.ts.net/jellyfin";
-                description = "Media server";
-              };
-            }
-            {
-              "Seerr" = {
-                icon = "seerr.png";
-                href = "https://periserver.tail0e3fc3.ts.net/seerr";
-                description = "Frères Lumières request management";
-              };
-            }
             {
               "Navidrome" = {
                 icon = "navidrome.png";
@@ -41,13 +27,6 @@
                 description = "Beeps and boops management";
               };
             }
-            # {
-            #   "Paperless" = {
-            #     icon = "paperless.png";
-            #     href = "https://periserver.tail0e3fc3.ts.net/paperless";
-            #     description = "Not Zotero";
-            #   };
-            # }
           ];
         }
         {
@@ -55,11 +34,11 @@
             {
               "FreshRSS" = {
                 icon = "freshrss.png";
-                href = "https://periserver.tail0e3fc3.ts.net/freshrss";
+                href = "https://server.ts.net/freshrss";
                 description = "RSS reader";
                 widget = {
                   type = "freshrss";
-                  url = "https://periserver.tail0e3fc3.ts.net/freshrss";
+                  url = "https://server.ts.net/freshrss";
                 };
               };
             }
@@ -73,7 +52,7 @@
             {
               "Full Text to Rss" = {
                 icon = "rsshub.png";
-                href = "https://periserver.tail0e3fc3.ts.net/ft-rss";
+                href = "https://server.ts.net/ft-rss";
                 description = "Get full text in RSS";
               };
             }
@@ -86,59 +65,6 @@
                 icon = "mdi-folder";
                 href = "/filebrowser/";
                 description = "File browser";
-              };
-            }
-          ];
-        }
-        {
-          "Arrrrrrrrrrrrrrrrrrrrrrrrr" = [
-            {
-              "Prowlarr" = {
-                icon = "prowlarr.png";
-                href = "/prowlarr/";
-                description = "Indexer manager";
-              };
-            }
-            {
-              "Sonarr" = {
-                icon = "sonarr.png";
-                href = "/sonarr/";
-                description = "TV show management";
-              };
-            }
-            {
-              "Radarr" = {
-                icon = "radarr.png";
-                href = "/radarr/";
-                description = "Movie management";
-              };
-            }
-            {
-              "Lidarr" = {
-                icon = "lidarr.png";
-                href = "/lidarr/";
-                description = "Music management";
-              };
-            }
-            {
-              "Whisparr" = {
-                icon = "Whisparr.png";
-                href = "/whisparr/";
-                description = "Seggs management";
-              };
-            }
-            {
-              "Bazarr" = {
-                icon = "bazarr.png";
-                href = "/bazarr/";
-                description = "Subtitles management";
-              };
-            }
-            {
-              "Qbittorrent" = {
-                icon = "qbittorrent.png";
-                href = "/qbt/";
-                description = "Torrent client";
               };
             }
           ];
